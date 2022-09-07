d42e50b871299d5b96c7692950594dad1b2a76b4
SUNNY BHAVEEN CHANDRA <sunny.c17hawke@gmail.com>
2022-09-08 03:41:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 REPO_NAME = "IPYNBrenderer"
 AUTHOR_USER_NAME = "c17hawke"
