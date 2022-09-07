b4d6c1077ee35282b7fee426484b35ef67957561
c17hawke <sunny.c17hawke@gmail.com>
2022-09-08 01:58:07 +0530

    updated after unit test

--- src/IPYNBrenderer/youtube.py
+++ src/IPYNBrenderer/youtube.py
@@ -16,6 +16,8 @@ def get_time_info(URL: str) -> int:
 
     try:
         split_val = URL.split("=")
+        if len(split_val) > 3:
+            raise InvalidURLException
         if "watch" in URL:
             if "&t" in URL:
                 vid_id, time = split_val[-2][:-2], int(split_val[-1][:-1])
